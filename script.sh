9e36ea6a829a79ba93402895960b5d4250246db2
crypticwisdom <crypticwisdom84@gmail.com>
2022-08-30 15:24:49 -0700

    Starting to code today, so cool

---
+++ script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Loading your script, please wait ..."
+
+echo \n"Scripthas been loaded, ..."
+
+echo "Enter name of folder to create: "
+read  name
+echo "you are creating '$name' "
+
